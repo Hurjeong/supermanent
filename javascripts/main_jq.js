c6aeb245793de164cc9d816c06945c50215ffbcc
Hurjeong <lyumin123@naver.com>
2013-11-21 01:57:35 +0900

    TT
    TT

--- javascripts/main_jq.js
+++ javascripts/main_jq.js
@@ -1,7 +1,4 @@
-var script = document.createElement('script');
-script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+
 
 var el = $("#one");
 var menu = $("#nav");
