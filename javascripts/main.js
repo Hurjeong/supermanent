6f50fe6f63719059af2dc55d429144294ee1b912
Hurjeong <lyumin123@naver.com>
2013-11-20 23:02:56 +0900

    d
    d

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,56 @@
-console.log('This would be the main JS file.');
+var el = $("#one");
+var menu = $("#nav");
+var menuItems = $(".menu > a");
+var pos = el.offset().top;
+didScroll = false;
+
+$(window).on("scroll", function() {
+  didScroll = true;
+});
+
+// http://ejohn.org/blog/learning-from-twitter/#postcomment
+setInterval(function() {
+  if(didScroll) {
+    didScroll = false;
+      var scrollPos = $(window).scrollTop();
+  
+  if (scrollPos > (pos - 50)) { //Adjust menu opening
+    menu.addClass('stick');
+  } 
+  else { 
+    menu.removeClass('stick');
+  }; 
+  $('.inview').each(function(){
+  var itemOffset = Math.abs($(this).offset().top);
+    if (itemOffset > 0 && itemOffset < scrollPos) {
+      var menuElm = $(this).attr('id');
+      console.log(menuElm);
+      menuItems.removeClass("active");
+      $("." + menuElm).addClass("active");
+    }
+  });     
+  }
+}, 250);
+
+/* Performs a smooth page scroll to an anchor on the same page.
+http://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 500);
+            return false;
+        }
+    }
+});
+
+
+
+
+
