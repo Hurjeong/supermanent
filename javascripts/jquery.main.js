c6993c2631b8cc46ff05e81d95f171e1a63a4aad
Hurjeong <lyumin123@naver.com>
2013-11-21 12:35:55 +0900

    please
    please

--- javascripts/jquery.main.js
+++ javascripts/jquery.main.js
@@ -5,6 +5,8 @@ var menuItems = $(".menu > a");
 var pos = el.offset().top;
 didScroll = false;
 
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
 <script>
 $(window).on("scroll", function() {
   didScroll = true;
