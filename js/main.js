4d1cf72fcb6a5bad011fcc2666c527dd47147f2b
Hurjeong <lyumin123@naver.com>
2013-11-28 04:16:06 +0900

    dd
    dd

--- js/main.js
+++ js/main.js
@@ -1,65 +1,16 @@
-var el = $("#one");
-var menu = $("#nav");
-var menuItems = $(".menu > a");
-var pos = el.offset().top;
-didScroll = false;
+var scene;
+var init = function(){
+    scene =document.querySelector('#scene');
+};
 
-$(window).on("scroll", function() {
-  didScroll = true;
-});
+window.onload = function(){
+    init();
+    
+}
 
+window.onmousemove = function(e){
+    var dx =window.innerWidth / 2 - e.pageX;
+    var s ='rotateY('+dx+'deg)';
+    scene.style['-webkit-transform'] = s;
 
-// http://ejohn.org/blog/learning-from-twitter/#postcomment
-setInterval(function() {
-  if(didScroll) {
-    didScroll = false;
-      var scrollPos = $(window).scrollTop();
-  
-  if (scrollPos > (pos - 50)) { //Adjust menu opening
-    menu.addClass('stick');
-  } 
-  else { 
-    menu.removeClass('stick');
-  }; 
-
-<script>
-
-  $('.inview').each(function(){
-  var itemOffset = Math.abs($(this).offset().top);
-    if (itemOffset > 0 && itemOffset < scrollPos) {
-      var menuElm = $(this).attr('id');
-      console.log(menuElm);
-      menuItems.removeClass("active");
-      $("." + menuElm).addClass("active");
-    }
-  });     
-  }
-}, 250);
-</script>
-
-/* Performs a smooth page scroll to an anchor on the same page.
-http://css-tricks.com/snippets/jquery/smooth-scrolling/ */
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script>
-
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 500);
-            return false;
-        }
-    }
-});
-
-
-</script>
-
-
-
-
+};
